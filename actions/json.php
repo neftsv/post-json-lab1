60bc034b6ad9dc240abb5e1268da4e7cd1b3ca57
Sviatoslav Kushnir <svkush2013@gmail.com>
2021-09-29 22:23:32 +0300

    feat(index.php, styles, actions/json.php): updating the core files.

--- actions/json.php
+++ actions/json.php
@@ -1,3 +1,20 @@
 <?php
-$date = date("d-m_Y");
-print ($date);
+
+if (!empty($_POST)) {
+  //print_r($_POST);
+  
+  $data = array(
+    'content' => array(
+      'date' => $_POST['calendar'],
+      'note' => $_POST['note'],
+    ),
+  );
+  
+  //$data = array_push($data['content']);
+  
+  $json = json_encode($data);
+  $file = 'output.json';
+  file_put_contents($file, $json, FILE_APPEND);
+  header('http://localhost/lab_1');
+  print('<h2>Data generated successfully. You can download by  <a href="http://localhost/lab_1/actions/'.$file.'">click</a></h2>');
+}
--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title> Note </title>
-		<link rel="stylesheet" href="Style.css" type="text/css">
-	</head>
-    <body>
-        <div>
-			<div>
-				<form action="server.php" method="post">
-					<p class="a">Виберіть дату: <input type="date" name="calendar" value="<?php echo date('Y-m-d');?>"></p>
-				</form>
-			</div>
-        </div>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <title> Note </title>
+  <link rel="stylesheet" href="css/style.css?v1" type="text/css">
+</head>
+<body>
+  <form action="actions/json.php" method="post">
+    <label for="date">
+      <span>Enter date: </span>
+      <input id="date" type="date" name="calendar" value="<?php echo date('d-m-Y'); ?>">
+    </label>
+    <label for="text-field">
+      <span>Insert note: </span>
+      <textarea id="text-field" name="note"><?php echo "Enter note"; ?></textarea>
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+</body>
 </html>
